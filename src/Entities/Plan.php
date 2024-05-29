9ea967704458f478aec1b5fcb29ce18daa5031c8
Bechir El Bechir <bechir@smartmssa.com>
2024-05-29 13:22:17 +0000

    Fix intervals relations to plan

--- src/Entities/Plan.php
+++ src/Entities/Plan.php
@@ -4,8 +4,10 @@ namespace Beksos\LaravelSubscriptions\Entities;
 
 use Beksos\LaravelSubscriptions\Plan as PlanBase;
 use Beksos\LaravelSubscriptions\Traits\HasSingleInterval;
+use Beksos\LaravelSubscriptions\Traits\HasManyIntervals;
 
 class Plan extends PlanBase
 {
     use HasSingleInterval;
+    use HasManyIntervals;
 }
