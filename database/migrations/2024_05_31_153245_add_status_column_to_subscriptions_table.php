f8806dc7a3cbc057ead8d70d888637fafdf1b059
Bechir El Bechir <bechir@smartmssa.com>
2024-05-31 15:39:36 +0000

    Subscription status attribute

--- database/migrations/2024_05_31_153245_add_status_column_to_subscriptions_table.php
+++ database/migrations/2024_05_31_153245_add_status_column_to_subscriptions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSubscriptionsTables extends Migration
+class UpdateSubscriptionsTables extends Migration
 {
     /**
      * Run the migrations.
